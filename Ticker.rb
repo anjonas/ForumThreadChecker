bb09ff7f05e4131bd77bc2d5b1fed5e6d3702845
Jonas Angesleva <anjonas@student.chalmers.se>
2011-10-28 19:07:52 +0200

    check_site can now parse the number of pages in a thread

--- Ticker.rb
+++ Ticker.rb
@@ -24,7 +24,12 @@ class Ticker
 
   def check_site()
 	count = 0
-    doc = Nokogiri::HTML(RestClient.get(@url))
+    docc = Nokogiri::HTML(RestClient.get("http://forum.bodybuilding.com/showthread.php?t=113190661"))
+	docc.css('span/a[@class = "popupctrl"]').each do |link|
+	  puts link.text
+	  #Trying to get the page counter, but I also get a lot of junk
+	end
+	doc = Nokogiri::HTML(RestClient.get(@url))
 	doc.css('a.postcounter').each do |node|
 	  count = count + 1
 	end
