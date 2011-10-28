1a9363cfa333373502f3fc4577267cbc47663b27
Jonas Angesleva <anjonas@student.chalmers.se>
2011-10-28 19:11:04 +0200

    cleared up check_site

--- Ticker.rb
+++ Ticker.rb
@@ -24,21 +24,22 @@ class Ticker
 
   def check_site()
 	count = 0
-    docc = Nokogiri::HTML(RestClient.get("http://forum.bodybuilding.com/showthread.php?t=113190661"))
-	docc.css('span/a[@class = "popupctrl"]').each do |link|
+    doc = Nokogiri::HTML(RestClient.get(@url))
+	
+	doc.css('span/a[@class = "popupctrl"]').each do |link|
 	  puts link.text
-	  #Trying to get the page counter, but I also get a lot of junk
 	end
-	doc = Nokogiri::HTML(RestClient.get(@url))
+	
 	doc.css('a.postcounter').each do |node|
 	  count = count + 1
 	end
 	puts count
+	
   end
 end
 
 def test
-  ticker = Ticker.new(1, "http://forum.bodybuilding.com/showthread.php?t=139217483")
+  ticker = Ticker.new(1, "http://forum.bodybuilding.com/showthread.php?t=113190661")
   ticker.check_site
   puts "testing"
 end
