c3bc6a48cb7dfaaed0c8ccf5b1a0b27b26c67aed
Jonas Angesleva <anjonas@student.chalmers.se>
2011-10-28 18:22:45 +0200

    check_site can now count the replies in a page

--- Ticker.rb
+++ Ticker.rb
@@ -23,13 +23,17 @@ class Ticker
   end
 
   def check_site()
-    doc = Nokogiri::HTML(RestClient.get("http://forum.bodybuilding.com/showthread.php?t=139217483"))
-	puts doc 
+	count = 0
+    doc = Nokogiri::HTML(RestClient.get(@url))
+	doc.css('a.postcounter').each do |node|
+	  count = count + 1
+	end
+	puts count
   end
 end
 
 def test
-  ticker = Ticker.new(1, "lol")
+  ticker = Ticker.new(1, "http://forum.bodybuilding.com/showthread.php?t=139217483")
   ticker.check_site
   puts "testing"
 end
